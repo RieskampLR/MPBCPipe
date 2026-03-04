c0abefb612053b56f12a2ac8a1cf7e52acfe0521
unknown <lea.rieskamp@gmail.com>
2026-03-04 18:00:11 +0100

    piper update

--- piper1.py
+++ piper1.py
@@ -8,6 +8,9 @@ piper1.py
 Description:
 
 
+Hard-code conditions:
+- Header entries / short codes have to be named the same in any new data collection files
+
 User-defined functions: None
 Non-standard modules: None
 
@@ -32,25 +35,42 @@ import sys
 import pandas as pd
 from pathlib import Path
 
+print("Hi")
+
 
 # Storing Paths of arguments in variables:
-    
+
+"""    
 qdat = pd.read_csv(Path(sys.argv[1])) # questionnaire data
 pdat = pd.read_table(Path(sys.argv[2]), encoding='unicode_escape') # pharmacy data
 hdat = pd.read_table(Path(sys.argv[3]), encoding='unicode_escape', low_memory=False) # hospital data
 vdat = pd.read_table(Path(sys.argv[4]), encoding='unicode_escape', low_memory=False) # visits data
+"""
 
-print(hdat)
-print (hdat[0:1])
-print(hdat["FLAND"])
+qdat = pd.read_table(Path("C:/Users/admin/OneDrive/Dokumente/UniLund/Thesis/dats/qdat_anonymised.tsv"))
+pdat = pd.read_table(Path("C:/Users/admin/OneDrive/Dokumente/UniLund/Thesis/dats/pdat_anonymised.tsv"), encoding='unicode_escape')
+hdat = pd.read_table(Path("C:/Users/admin/OneDrive/Dokumente/UniLund/Thesis/dats/hdat_anonymised.tsv"), encoding='unicode_escape', low_memory=False)
+vdat = pd.read_table(Path("C:/Users/admin/OneDrive/Dokumente/UniLund/Thesis/dats/vdat_anonymised.tsv"), encoding='unicode_escape', low_memory=False)
 
 
 # Input file checks
 # ...
-    
+  
+
+# List of diagnoses present in data sets
+
+diagnoses = []
+for col in hdat.loc[:,"DIA1":]:     # loops over dia coloumns
+    for dia in hdat[col]:           # loops over the current column entries
+        if not pd.isna(dia):        # excludes empty entries
+            if dia not in diagnoses:
+                diagnoses.append(dia)
+#print(diagnoses)
+
+
 
 # Combine data to one table
 
-
+thetable = 
 
 
