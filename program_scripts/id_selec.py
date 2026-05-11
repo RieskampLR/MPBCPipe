e18b0e5a5ee7156df838dc4b178ac423444d1949
unknown <lea.rieskamp@gmail.com>
2026-05-11 16:00:15 +0200

    id_selec fix for real StudieIDs

--- program_scripts/id_selec.py
+++ program_scripts/id_selec.py
@@ -62,11 +62,11 @@ def id_selection_func(tables, cond):
             else:
                 ids = ids[ids[col].isin(val["values"])]
                     
+        ids["StudieID"] = ids["StudieID"].apply(lambda x: x[0] if isinstance(x, list) else x) # Ensuring all IDs are string entries (not lists)
         id_selection[table] = ids["StudieID"]
 
 
     # IDs must match ALL conditions
-    ids["StudieID"] = ids["StudieID"].apply(lambda x: x[0] if isinstance(x, list) else x) # Ensuring all IDs are string entries (not lists)
     common_ids = set.intersection(*(set(v) for v in id_selection.values()))
     
     
